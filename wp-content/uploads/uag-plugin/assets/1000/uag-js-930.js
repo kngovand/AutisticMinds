50a3a48b3a68ef7e492d73f2b81fec584cbd6408
Kevin Duc <kngovand@gmail.com>
2025-04-01 15:25:13 -0600

    init, design done

---
+++ wp-content/uploads/uag-plugin/assets/1000/uag-js-930.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener("DOMContentLoaded", function(){
+	UAGBForms.init( {"block_id":"a2769835","reCaptchaEnable":false,"reCaptchaType":"v2","reCaptchaSiteKeyV2":"","reCaptchaSecretKeyV2":"","reCaptchaSiteKeyV3":"","reCaptchaSecretKeyV3":"","afterSubmitToEmail":"autisticmindsco@gmail.com","afterSubmitCcEmail":"","afterSubmitBccEmail":"","afterSubmitEmailSubject":"Form Submission - Sponsor","sendAfterSubmitEmail":true,"confirmationType":"message","hidereCaptchaBatch":false,"captchaMessage":"Please fill up the above captcha.","confirmationUrl":""}, '.uagb-block-a2769835', 930 );
+});
+ });
